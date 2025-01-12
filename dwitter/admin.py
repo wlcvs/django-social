772377e105d36a1e809914d7cf88a43cd5239cd8
Wallacy V. <wlcsv8080@outlook.com>
2025-01-12 13:17:40 -0300

    Eu já fiz esse projeto antes

---
+++ dwitter/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+
+from .models import Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
