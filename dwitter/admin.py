413e0f8b6cb55d1a64021da882003bf1f05996eb
Wallacy V. <wlcsv8080@outlook.com>
2025-01-13 18:29:44 -0300

    Step eight completed

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
-from .models import Profile
+from .models import Profile, Dweet
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -15,3 +15,4 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
+admin.site.register(Dweet)
--- dwitter/models.py
+++ dwitter/models.py
@@ -25,3 +25,17 @@ def create_profile(sender, instance, created, **kwargs):
 
 # Create a Profile for each new user.
 # Remove: post_save.connect(create_profile, sender=User)
+
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User, related_name="dweets", on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%d-%m-%Y %H:%M}): "
+            f"{self.body[:30]}..."
+        )
